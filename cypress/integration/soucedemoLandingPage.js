6af746b7ce5553e20663e4e6ba0c33efaa0ce199
kmlynski <kamil.mlynski1@gmail.com>
2021-01-24 13:31:33 +0100

    Added baseUrl parameter to config

--- cypress/integration/soucedemoLandingPage.js
+++ cypress/integration/soucedemoLandingPage.js
@@ -1,11 +1,10 @@
 /// <reference types="cypress" />
 
-it('should navigate to the landing page', () => {
-    cy.visit('https://www.saucedemo.com/')
+beforeEach(() =>{
+    cy.visit('/')
 })
 
 it('should be able to log in as standard_user', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test=username]').type('standard_user')
     cy.get('[data-test=password]').type('secret_sauce')
     cy.get('#login-button').click()
@@ -15,7 +14,6 @@ it('should be able to log in as standard_user', () => {
 })
 
 it('should display error on incorrect credentials', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test=username]').type('incorrect')
     cy.get('[data-test=password]').type('credentials')
     cy.get('#login-button').click()
@@ -25,7 +23,6 @@ it('should display error on incorrect credentials', () => {
 })
 
 it('locked_out_user should not be able to log in', () => {
-    cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test=username]').type('locked_out_user')
     cy.get('[data-test=password]').type('secret_sauce')
     cy.get('#login-button').click()
@@ -35,7 +32,6 @@ it('locked_out_user should not be able to log in', () => {
 })
 
 it('should not be able to log in with empty password', () => {
-    cy.visit('https://www.saucedemo.com/')
 
     cy.get('[data-test=username]').type('locked_out_user')
     cy.get('#login-button').click()
@@ -44,7 +40,6 @@ it('should not be able to log in with empty password', () => {
     .should('contain.text','Password is required')
 })
 it('should not be able to log in with empty username', () => {
-    cy.visit('https://www.saucedemo.com/')
 
     cy.get('[data-test=password]').type('secret_sauce')
     cy.get('#login-button').click()
